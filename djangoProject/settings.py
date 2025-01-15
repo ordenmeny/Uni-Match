523ee26e69e363573dfa1c3f82d0b929fae718d6
Dmitriy Alexandrov <newsmobileads@gmail.com>
2025-01-15 22:54:59 +0500

    Добавлена привязка тг-бота к аккаунту пользователя.

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
+from .api_views import *
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(), name="logout"),
+    path('login/', CustomLoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('get-bot/', GetBotView.as_view()),
+    path('api/get-bot/<slug:uniq_code>/', GetUserAPIView.as_view()),
 ]
